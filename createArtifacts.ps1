79d7978fe78ddf7255af2fe1aea1bfc56a511286
Magdziak Marek <marek.magdziak@tieto.com>
2016-07-16 20:17:12 +0200

    Set up autodeploy

--- createArtifacts.ps1
+++ createArtifacts.ps1
@@ -1,4 +1,4 @@
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet pack .\src\GraphQLCore -c Release -o .\artifacts --version-suffix=$revision }
+dotnet pack .\src\GraphQLCore -c Release -o .\artifacts --version-suffix=$revision
